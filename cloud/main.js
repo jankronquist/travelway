61debb0c19c147ce2b311ee9bc7cddea54e86e99
Jan Kronquist <jan.kronquist@gmail.com>
2015-10-02 18:07:22 +0200

    Added mailgun

--- cloud/main.js
+++ cloud/main.js
@@ -5,3 +5,25 @@ require('cloud/app.js');
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+var Mailgun = require('mailgun');
+Mailgun.initialize('sandboxc443a858bd5c4fe3a9ea2af18f38956e.mailgun.org', 'key-843c638b561552eef76475835c5a25d4');
+
+Parse.Cloud.define("email", function(request, response) {
+  Mailgun.sendEmail({
+    to: "jan.kronquist@gmail.com",
+    from: "Mailgun@CloudCode.com",
+    subject: "Hello from Cloud Code!",
+    text: "Using Parse and Mailgun is great!"
+  }, {
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      response.success("Email sent!");
+    },
+    error: function(httpResponse) {
+      console.error(httpResponse);
+      response.error("Uh oh, something went wrong");
+    }
+  });
+});
+
